d392eff8a88b288b0442dc84ea54a03386f77716
Victor Praxedes <victor.praxedes@gmail.com>
2019-08-14 22:34:44 -0300

    fixed wrong code so i stop getting errors on log, also printing guilds in console at startup

--- bot.py
+++ bot.py
@@ -8,14 +8,17 @@ bot = commands.Bot(command_prefix='.', description=description)
 client = discord.Client()
 token = getenv('BOT_TOKEN')
 
-bt_role = discord.utils.get(client.get_guild().roles, name='Boss Timer')
+# bt_role = discord.utils.get(client.get_guild().roles, name='Boss Timer')
 
-@bot.event
+@client.event
 async def on_ready():
     print('Bot ID: ', bot.user.id)
     print('Bot name: ', bot.user.name)
     print('---------------')
     print('This bot is ready for action!')
+    for guild in client.guilds:
+    	print(guild)
+    	pass
 
 @bot.command()
 async def peixinho(ctx):
@@ -26,20 +29,22 @@ async def peixinho(ctx):
 async def notifyme(ctx):
 	'''Adiciona seu nome na lista de avisos'''
 	user = ctx.message.author
-	await user.add_roles(bt_role)
+	role = discord.utils.get(ctx.guild.roles.roles, name='Boss Timer')
+	await user.add_roles()
 	await ctx.send('Você será notificado na hora de um boss :)')
 
 @bot.command()
 async def removeme(ctx):
 	'''Remove seu nome da lista de avisos'''
 	user = ctx.message.author
-	await user.remove_roles(bt_role)
+	role = discord.utils.get(ctx.guild.roles.roles, name='Boss Timer')
+	await user.remove_roles()
 	await ctx.send('Você **não será mais** notificado na hora de um boss :)')
 
 boss_about2_spawn == True
 
 if boss_about2_spawn:
-	channel.send(bt_role.mention + " alooo")
+	channel.send("one shot one kill")
 	boss_about2_spawn = False
 	pass
 
