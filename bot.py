8b0a6b9dab6aaa3f0479b04233e66c62fea8ea9e
Victor Praxedes <victor.praxedes@gmail.com>
2019-08-15 16:30:01 -0300

    Created an async task that prints msg to console every 1m as a poc

--- bot.py
+++ bot.py
@@ -1,23 +1,22 @@
 import datetime
+import asyncio
 import discord
-from os import getenv
 from discord.ext import commands
+from os import getenv
 
 description = 'Bot para mandar alertas dos boss do servidor South America'
 bot = commands.Bot(command_prefix='.', description=description)
 token = getenv('BOT_TOKEN')
 
-# bt_role = discord.utils.get(client.get_guild().roles, name='Boss Timer')
-
 @bot.event
 async def on_ready():
-    print('Bot ID: ', bot.user.id)
-    print('Bot name: ', bot.user.name)
-    print('---------------')
-    print('This bot is ready for action!')
-    for guild in bot.guilds:
-    	print(guild)
-    	pass
+	print('Bot ID: ', bot.user.id)
+	print('Bot name: ', bot.user.name)
+	print('---------------')
+	print('This bot is ready for action!')
+	print('I\'m currently on those servers: ')
+	for guild in bot.guilds:
+		print(guild)
 
 @bot.command()
 async def peixinho(ctx):
@@ -39,5 +38,14 @@ async def removeme(ctx):
 	role = discord.utils.get(ctx.guild.roles.roles, name='Boss Timer')
 	await user.remove_roles()
 	await ctx.send('Você **não será mais** notificado na hora de um boss :)')
+
+@bot.event
+async def background_task():
+	await bot.wait_until_ready()
+	while not bot.is_closed():
+		print('1 minute counter')
+		await asyncio.sleep(60) # task runs every 60 seconds
+
+bot.bg_task = bot.loop.create_task(background_task())
 
 bot.run(token)
